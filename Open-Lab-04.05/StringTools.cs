7edd4619b313bc592c50abf7a9cb405d88824cf4
Viktor <krpectv57@gmail.com>
2020-10-24 00:31:54 +0200

    _

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Open_Lab_04._05
 {
     public class StringTools
     {
         public string Repeat(string orig, int n)
-        {
-            throw new NotImplementedException();
-        }
+        {            
+            string a = "";
+            for (int i = 0; i < orig.Length; i++)
+            {
+                for (int y=0; y<n; y++)
+                {                   
+                        a = a + orig[i];                    
+                }
+            }            
+            return a.ToString();
+        }               
     }
 }
